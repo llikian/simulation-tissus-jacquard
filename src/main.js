ab43c8ffaf41b3e7dfd30313c6a95c55c871531c
llikian <pokillian64@gmail.com>
2025-11-13 16:17:49 +0100

    jeu. 13 nov. 2025 15:51:12 CET

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
-import {Vector3} from 'three';
-import {FirstPersonControls} from "three/addons/controls/FirstPersonControls";
+import {OrbitControls} from "three/addons/controls/OrbitControls";
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -22,64 +21,17 @@ const cube = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1),
     new THREE.MeshLambertMaterial({color: 0xff0000})
 );
-cube.position.z = -5.0;
 scene.add(cube);
 
-// const geometry = new THREE.BufferGeometry();
-// const mesh = new THREE.Mesh(
-//     geometry,
-//     new THREE.MeshLambertMaterial({color: 0x00ffff})
-// );
-// scene.add(mesh);
-// mesh.position.z = -5.0;
-
-// const WORLD_UP = new Vector3(0.0, 1.0, 0.0);
-//
-// document.addEventListener("keypress", (event) => {
-//     const MOVEMENT_SPEED = 0.1;
-//     const direction = new Vector3();
-//
-//     switch (event.key) {
-//         case "z":
-//             camera.getWorldDirection(direction);
-//             camera.position.add(direction.multiplyScalar(MOVEMENT_SPEED));
-//             break;
-//         case "s":
-//             camera.getWorldDirection(direction);
-//             camera.position.add(direction.multiplyScalar(-MOVEMENT_SPEED));
-//             break;
-//         case "d":
-//             camera.getWorldDirection(direction);
-//             camera.position.add(direction.cross(WORLD_UP).multiplyScalar(MOVEMENT_SPEED));
-//             break;
-//         case "q":
-//             camera.getWorldDirection(direction);
-//             camera.position.add(direction.cross(WORLD_UP).multiplyScalar(-MOVEMENT_SPEED));
-//             break;
-//     }
-// });
-//
-// document.addEventListener("mousemove", (event) => {
-//     const SPEED = 0.01;
-//     const MAX_ANGLE = Math.PI / 2.0;
-//
-//     camera.rotation.y -= SPEED * event.movementX;
-//     camera.rotation.x -= SPEED * event.movementY;
-//     camera.rotation.x = Math.clamp(camera.rotation.x, -MAX_ANGLE, MAX_ANGLE);
-// });
-
-const controls = new FirstPersonControls(camera, renderer.domElement);
-controls.lookSpeed = 0.1;
-controls.movementSpeed = 1.0;
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 0, 0);
+camera.position.set( 0, 3, 3 );
+controls.update();
 
 function animate() {
     const delta = clock.getDelta();
 
     controls.update(delta);
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
     renderer.render(scene, camera);
 }
 
